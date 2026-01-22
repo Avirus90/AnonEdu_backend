405b8d39f8f5c25178bd4c390460e4b2146fcbf5
Avirus90 <115672412+Avirus90@users.noreply.github.com>
2026-01-23 02:50:08 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,1 +1,13 @@
-nothung
+const express = require("express");
+const app = express();
+
+// simple route
+app.get("/", (req, res) => {
+  res.send("Server chal raha hai 🚀");
+});
+
+// server start
+const PORT = 3000;
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
